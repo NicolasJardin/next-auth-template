4cb717980280c5969bb64a3c9c7a8b780eae48f5
nicolasmoovin <nicolasjardindosantos@gmail.com>
2024-12-05 18:01:06 -0300

    feat: Implementar tema light e dark conforme sistema

--- src/components/app/app-providers/AppProviders.tsx
+++ src/components/app/app-providers/AppProviders.tsx
@@ -1,9 +1,17 @@
 'use client'
+
+import { ThemeProvider } from '@/components/theme'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PropsWithChildren } from 'react'
 
 const queryClient = new QueryClient()
 
 export function AppProviders({ children }: PropsWithChildren) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  )
 }
